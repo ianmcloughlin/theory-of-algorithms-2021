c9f2e47affb44ec6e1695a42f5490b0f23282423
Ian McLoughlin <mcloughlin.ian@gmail.com>
2021-01-30 19:26:16 +0000

    Video stopped

--- bitoperations.c
+++ bitoperations.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+void bit_print(int i) {
+    for (int j = 0; j < 8; j++)
+        printf("%d", ((1 << j) & i) ? 1 : 0);
+}
+
 int main(int argc, char *argv[]) {
     
-    // Print Hello, world!
-    printf("%s\n", "Hello, world again!");
+    int i = 1;
+
+    for (int j = 0; j < 8; j++){
+        bit_print(i << j);
+        printf("\n");
+    }
     
     return 0;
 }
