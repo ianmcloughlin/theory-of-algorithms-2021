aee31d54ff378018131162e37fe722b852b7bcb8
Ian McLoughlin <mcloughlin.ian@gmail.com>
2021-01-31 13:06:29 +0000

    End of week 2

--- bitoperations.c
+++ bitoperations.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
+void bin_print(int i) {
+    // Number of bis in an integer.
+    int j = sizeof(int) * 8;
+
+    // Temporary variable.
+    int k;
+
+    for (j-- ; j >= 0; j--) {
+        k = ((1 << j) & i) ? 1 : 0;
+        printf("%d", k);
+    }
+
+}
+
 int main(int argc, char *argv[]) {
     
-    int i = 0xF1; // 11110001
+    int i = 0xf1;
 
-    printf("Dec: %d\n", i);
-    printf("Hex: %X\n", i);
+    printf("Original:  ");
+    bin_print(i);
+    printf("\n");
 
-    printf("Size of i:    %d\n", sizeof(i));
-    printf("Size of int:  %d\n", sizeof(int));
-    printf("Size of char: %d\n", sizeof(char));
-
-    char c = 41;
-    printf("c in char is: %c\n", c);
-    printf("c in int  is: %d\n", c);
-
-    int j = 1000000000;
-    printf("j in int  is: %d\n", j);
-    printf("j in char is: %c\n", (char) j);
-    printf("j in int from char is: %d\n", (int) (char) j);
+    for (int j = 0; j < 40; j++) {
+        printf("%3d << %2d: ", i, j);
+        bin_print(i << j);
+        printf("\n");
+    }
 
     return 0;
 }
