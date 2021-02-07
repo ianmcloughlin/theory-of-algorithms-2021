bfbbed449af32b7199fa1013f41addfd4b1ce447
Ian McLoughlin <mcloughlin.ian@gmail.com>
2021-02-07 14:14:53 +0000

    Bit masks

--- bitoperations.c
+++ bitoperations.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 
-void bin_print(int i) {
+void bin_print(unsigned int i) {
     // Number of bis in an integer.
-    int j = sizeof(int) * 8;
+    int j = sizeof(unsigned int) * 8;
 
     // Temporary variable.
     int k;
 
+    // Loop over the number of bits in i, left to right.
     for (j-- ; j >= 0; j--) {
+        // Pick out the j^th bit of i.
         k = ((1 << j) & i) ? 1 : 0;
+        // Print k.
         printf("%d", k);
     }
-
 }
 
+// Standard main signature.
 int main(int argc, char *argv[]) {
-    
-    int i = 0xf1;
+    // Set i to a literal value.
+    unsigned int i = 0x0f0f0f0f;//1; // 0xf1; // 241 // 4294967295
 
-    printf("Original:  ");
+    // What we're printing.
+    printf("Original:\t");
+    // Print i in binary.
     bin_print(i);
-    printf("\n");
+    // End line.
+    printf("\t%x\t%u\n\n", i, i);
 
-    for (int j = 0; j < 40; j++) {
-        printf("%3d << %2d: ", i, j);
-        bin_print(i << j);
-        printf("\n");
+    // 32.
+    int j = sizeof(unsigned int) * 8;
+
+    for (j--; j >= 0 ; j--) {
+        // 1 shifted left j times.
+        bin_print(1 << j); printf("\n");
+        // i.
+        bin_print(i); printf("\n");
+        printf("-------------------------------- &\n");
+        // (1 shifted left j times) bitwise logical and i.
+        bin_print((1 << j) & i); printf("\n\n");
     }
 
+    // Everything is okay.
     return 0;
 }
